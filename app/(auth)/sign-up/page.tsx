194a29402a235c6e7ef9d270f6cd6547d20b62e9
bercivarga <65171545+bercivarga@users.noreply.github.com>
2023-12-05 20:51:16 +0100

    Finalize sign-up and new-user flow

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,13 +1,11 @@
-import {SignUp} from "@clerk/nextjs";
-import {Metadata} from "next";
+import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: 'Sign up',
-    description: 'Create an account',
-}
+  title: "Sign up",
+  description: "Create an account",
+};
 
 export default function SignUpPage() {
-  return (
-      <SignUp path="/sign-up" signInUrl="/sign-in" routing="path" />
-  )
+  return <SignUp path="/sign-up" signInUrl="/sign-in" routing="path" />;
 }
--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,7 +1,45 @@
-export default function NewUserPage() {
-    return (
-        <main>
-            <h1>Hi New User!</h1>
-        </main>
-    )
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+// Handles the process between creating a new user in Clerk
+// and creating a new user entry in the PostgreSQL database.
+async function createNewUser() {
+  const user = await currentUser();
+
+  if (!user) {
+    // For now, this edge case isn't handled, nor is it likely to happen.
+    throw new Error("No user found");
+  }
+
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user.id,
+    },
+  });
+
+  if (!match) {
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+      },
+    });
+  }
+
+  redirect("/journal");
+}
+
+// This could also be turned into a serverless function / api route,
+// but for practice's sake this isn't necessary.
+export default async function NewUserPage() {
+  await createNewUser();
+
+  return (
+    <main className="w-screen h-screen flex bg-gray-900">
+      <span className="block self-center text-2lg text-white">
+        ⏳ Creating your account...
+      </span>
+    </main>
+  );
 }
